51ec82281c17c94d71563cc0b05a1bb6bab5d1cc
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2018-09-04 14:33:54 -0500

    parte 1 terminada

--- src/main/resources/OrderController.js
+++ src/main/resources/OrderController.js
@@ -4,6 +4,27 @@
  * and open the template in the editor.
  */
 {
+    "order_id": 1,
+    "table_id": 1,
+    "products": [{
+    "product": "PIZZA",
+    "quantity": 3,
+    "price": "$10000"
+    },
+    {
+    "product": "HOTDOG",
+    "quantity": 1,
+    "price": "$3000"
+    },
+    {
+    "product": "COKE",
+    "quantity": 4,
+    "price": "$13000"
+    }
+    ]
+}
+
+function addOrder(){
     
 }
 
