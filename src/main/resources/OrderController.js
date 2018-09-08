8759ce17d5c9cb515b49523ea4fc8c4797db967e
DianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2018-09-08 14:28:24 -0500

    punto 2.3 completo

--- src/main/resources/OrderController.js
+++ src/main/resources/OrderController.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-{
+var order1 = {
     "order_id": 1,
     "table_id": 1,
     "products": [{
@@ -22,10 +22,39 @@
     "price": "$13000"
     }
     ]
-}
+};
 
 function addOrder(){
-    
+    var body = document.getElementById('Tabla');
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    var tr1= document.createElement('tr');
+    var td1 = document.createElement('td');
+    td1.appendChild(document.createTextNode('Product'));
+    tr1.appendChild(td1);
+    var td2 = document.createElement('td');
+    td2.appendChild(document.createTextNode('Quantity'));
+    tr1.appendChild(td2);
+    var td3 = document.createElement('td');
+    td3.appendChild(document.createTextNode('Price'));
+    tr1.appendChild(td3);
+    tbl.appendChild(tr1);
+    for (var i = 0; i < 3; i++) {
+        var tr = document.createElement('tr');
+        
+        var td4 = document.createElement('td');
+        td4.appendChild(document.createTextNode('PIZZA'));
+        tr.appendChild(td4);
+        var td5 = document.createElement('td');
+        td5.appendChild(document.createTextNode('3'));
+        tr.appendChild(td5);
+        var td6 = document.createElement('td');
+        td6.appendChild(document.createTextNode('10000'));
+        tr.appendChild(td6);
+            
+        tbl.appendChild(tr);
+    }
+    body.appendChild(tbl);
 }
 
 
